67801d2a4100b7992f09915cfa8fe18f1ede257a
Fabian Gamboa <fabiangamboa95@gmail.com>
2023-08-15 22:39:14 -0400

    + header, +contexts, +data

---
+++ lib/data.tsx
@@ -0,0 +1,82 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  // {
+  //     name: "Projects",
+  //     hash: "#projects",
+  // },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const skillsData = [
+  // main Languages
+  "Typescript",
+  "JavaScript",
+  "Python",
+  "Dart",
+
+  // Main Skills
+  "Fullstack",
+  "Frontend",
+  "Backend",
+  "Mobile",
+  "Software Design",
+  "Design Patterns",
+  "Functional Programming",
+  "DDD",
+  "Stratified Design",
+  "Clean Code",
+  "Architectural Design",
+  "Onion Architecture",
+  "SOLID",
+  "OOP",
+  "Algorithms Design",
+  "Systems Design",
+
+  // Main Frameworks / Libraries
+  "Nodejs",
+  "React",
+  "NextJs",
+  "Zustand",
+  "Tailwindcss",
+  "Antd",
+  "NestJs",
+  "KoaJs",
+  "ExpressJs",
+  "REST",
+  "GraphQL",
+  "tRPC",
+  "React Native",
+  "Ionic",
+
+  // Basic
+  "HTML",
+  "CSS",
+  "Git",
+  "Docker",
+  "CI/CD",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "PostgresSQL",
+  "Framer Motion",
+] as const;
